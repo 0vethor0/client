d4b37f3bcb49167b89353ce2f123f128f94df673
PrimerCheckPoint <uchija.shinui@gmail.com>
2025-02-25 17:34:39 -0400

    red

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
 import './App.css';
-import {useState} from 'react';
-
 function App() {
 
-  const [nombre, setNombre] = useState('');
-  const [apellido, setApellido] = useState('');
-  const [edad, setEdad] = useState(0);
-  const [correo, setCorreo] = useState(''); 
-  return (
-    <div className="App">
-        <div className="datos">
-              <label>Nombre:<input 
-                    onChange={(event) => {setNombre(event.target.value)}}
-                    type="text"/>
-              </label><br/>
-              <label>Apellido:<input 
-                    onChange={(event) => {setApellido(event.target.value)}}
-              type="text"/></label><br/>
-              <label>Edad:<input 
-                    onChange={(event) => {setEdad(event.target.value)}}
-              type="number"/></label><br/>
-              <label>Correo: <input
-                    onChange={(event) => {setCorreo(event.target.value)}}
-              type="email"/></label><br/>
-              <button>Registrar </button>
-        </div>
-    </div>
-  );
+return (
+<div className="App">
+      <div className="datos">
+            <label>Nombre:<input type="text"/></label><br/>
+            <label>Apellido:<input type="text"/></label><br/>
+            <label>Edad:<input type="number"/></label><br/>
+            <label>Correo: <input type="email"/></label><br/>
+            <button>Registrar </button>
+      </div>
+</div>
+);
 }
 
 export default App;
